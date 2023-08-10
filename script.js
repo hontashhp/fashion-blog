4b928466319d163e7f6836d00859f7d8b1e8d9ea
hontashhp <141926393+hontashhp@users.noreply.github.com>
2023-08-10 20:52:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,27 @@
-/* Reset some default styles */
-body, h1, p, ul {
-    margin: 0;
-    padding: 0;
+// Function to fetch and load JSON data into the card-container
+function loadCards() {
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            const cardContainer = document.querySelector('.card-container');
+
+            data.forEach(item => {
+                const card = document.createElement('div');
+                card.classList.add('card');
+
+                const title = document.createElement('h2');
+                title.textContent = item.title;
+
+                const content = document.createElement('p');
+                content.textContent = item.content;
+
+                card.appendChild(title);
+                card.appendChild(content);
+                cardContainer.appendChild(card);
+            });
+        })
+        .catch(error => console.error('Error loading JSON data:', error));
 }
 
-/* Set a background color and center the header image */
-header {
-    background-color: #f4f4f4;
-    text-align: center;
-}
-header img {
-    width: 100%;
-    max-width: 100%;
-    height: auto;
-}
-
-/* Create a two-column card layout */
-.card-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    padding: 20px;
-    margin: 20px auto;
-    max-width: 1200px;
-}
-
-.card {
-    background-color: #fff;
-    border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    width: calc(50% - 20px);
-    margin-bottom: 20px;
-    padding: 20px;
-    box-sizing: border-box;
-}
-
-/* Style for card content */
-.card h2 {
-    font-size: 24px;
-    margin-bottom: 10px;
-}
-
-.card p {
-    font-size: 16px;
-}
+// Call the loadCards function when the page is loaded
+window.addEventListener('load', loadCards);
