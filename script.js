7889973212c5ec857443f2bfe2d46cadd7073295
hontashhp <141926393+hontashhp@users.noreply.github.com>
2023-08-10 22:03:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// Function to create a card with the specified data
+function createCard(item) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    // Create the image element and set attributes
+    const image = document.createElement('img');
+    image.src = item.image;
+    image.alt = item.title; // Set the alt attribute for accessibility
+
+    const title = document.createElement('h2');
+    title.textContent = item.title;
+
+    const content = document.createElement('p');
+    content.textContent = item.content;
+
+    // Append the image, title, and content to the card
+    card.appendChild(image);
+    card.appendChild(title);
+    card.appendChild(content);
+
+    return card;
+}
+
 // Function to fetch and load JSON data into the card-container
 function loadCards() {
     fetch('data.json')
@@ -6,24 +30,7 @@ function loadCards() {
             const cardContainer = document.querySelector('.card-container');
 
             data.forEach(item => {
-                const card = document.createElement('div');
-                card.classList.add('card');
-
-                // Create the image element and set attributes
-                const image = document.createElement('img');
-                image.src = item.image;
-                image.alt = item.title; // Set the alt attribute for accessibility
-
-                const title = document.createElement('h2');
-                title.textContent = item.title;
-
-                const content = document.createElement('p');
-                content.textContent = item.content;
-
-                // Append the image, title, and content to the card
-                card.appendChild(image);
-                card.appendChild(title);
-                card.appendChild(content);
+                const card = createCard(item);
                 cardContainer.appendChild(card);
             });
         })
